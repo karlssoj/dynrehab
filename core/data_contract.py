ed9c3d50781c1a6ba27d488196b912a545c18eec
Jonny Karlsson <karlssoj@arcada.fi>
2026-03-27 12:09:50 +0200

    refactor: use dataclasses.asdict in PoseFrame.to_dict() to prevent field drift

--- core/data_contract.py
+++ core/data_contract.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 
 
 @dataclass
@@ -24,24 +24,4 @@ class PoseFrame:
     keypoints: dict = field(default_factory=dict)
 
     def to_dict(self) -> dict:
-        return {
-            "timestamp": self.timestamp,
-            "left_knee_angle": self.left_knee_angle,
-            "right_knee_angle": self.right_knee_angle,
-            "left_hip_angle": self.left_hip_angle,
-            "right_hip_angle": self.right_hip_angle,
-            "left_ankle_angle": self.left_ankle_angle,
-            "right_ankle_angle": self.right_ankle_angle,
-            "left_shoulder_angle": self.left_shoulder_angle,
-            "right_shoulder_angle": self.right_shoulder_angle,
-            "left_elbow_angle": self.left_elbow_angle,
-            "right_elbow_angle": self.right_elbow_angle,
-            "left_wrist_angle": self.left_wrist_angle,
-            "right_wrist_angle": self.right_wrist_angle,
-            "trunk_lean_angle": self.trunk_lean_angle,
-            "neck_angle": self.neck_angle,
-            "pelvic_tilt": self.pelvic_tilt,
-            "left_hka_alignment": self.left_hka_alignment,
-            "right_hka_alignment": self.right_hka_alignment,
-            "keypoints": self.keypoints,
-        }
+        return asdict(self)
